110fc35a8386f08ade405073f04d97dfe6856326
takusan23 <takumu1200@gmail.com>
2023-08-13 23:57:55 +0900

    小さいウィジェットの時にテキストを非表示にするように

--- app/src/main/java/io/github/takusan23/materialbatterywidget/MaterialBatteryWidget.kt
+++ app/src/main/java/io/github/takusan23/materialbatterywidget/MaterialBatteryWidget.kt
@@ -7,6 +7,8 @@ import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
 import android.os.Build
+import android.util.SizeF
+import android.view.View
 import android.widget.RemoteViews
 
 /**
@@ -50,7 +52,8 @@ class MaterialBatteryWidget : AppWidgetProvider() {
             val component = ComponentName(context, MaterialBatteryWidget::class.java)
             val widgetIdList = appWidgetManager.getAppWidgetIds(component)
             widgetIdList.forEach { id ->
-                val view = RemoteViews(context.packageName, R.layout.widget_material_battery).apply {
+                // 標準
+                val remoteView = RemoteViews(context.packageName, R.layout.widget_material_battery).apply {
                     // 電池残量を取得
                     val smartphoneBatteryData = BatteryUtil.getDeviceBatteryData(context)
                     val bluetoothBatteryData = BatteryUtil.getBluetoothBatteryData(context)
@@ -62,13 +65,49 @@ class MaterialBatteryWidget : AppWidgetProvider() {
                     setTextViewText(R.id.material_battery_widget_bluetooth_name_textview, bluetoothBatteryData?.name ?: context.getString(R.string.disconnected))
                     setTextViewText(R.id.material_battery_widget_bluetooth_battery_textview, bluetoothBatteryData?.level?.let { "$it%" } ?: "")
                     setProgressBar(R.id.material_battery_widget_bluetooth_progressbar, 100, bluetoothBatteryData?.level ?: 0, false)
+                    // smallView のときの Visibility を引き継ぐらしいので消す
+                    setViewVisibility(R.id.material_battery_widget_device_name_textview, View.VISIBLE)
+                    setViewVisibility(R.id.material_battery_widget_device_battery_textview, View.VISIBLE)
+                    setViewVisibility(R.id.material_battery_widget_bluetooth_name_textview, View.VISIBLE)
+                    setViewVisibility(R.id.material_battery_widget_bluetooth_battery_textview, View.VISIBLE)
                     // 押したら再読み込みするようにBroadcastを設定する
-                    val pendingIntent = PendingIntent.getBroadcast(context, PENDING_INTENT_REQUEST_CODE, Intent(context, MaterialBatteryWidget::class.java),
-                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else PendingIntent.FLAG_UPDATE_CURRENT)
+                    val pendingIntent = PendingIntent.getBroadcast(
+                        context, PENDING_INTENT_REQUEST_CODE, Intent(context, MaterialBatteryWidget::class.java),
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) PendingIntent.FLAG_IMMUTABLE else PendingIntent.FLAG_UPDATE_CURRENT
+                    )
                     setOnClickPendingIntent(R.id.material_battery_widget_parent, pendingIntent)
                 }
-                // 更新
-                appWidgetManager.updateAppWidget(id, view)
+                // レスポンシブデザインにする
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                    // 小さいウィジェット
+                    val smallView = RemoteViews(remoteView).apply {
+                        setViewVisibility(R.id.material_battery_widget_device_name_textview, View.GONE)
+                        setViewVisibility(R.id.material_battery_widget_device_battery_textview, View.GONE)
+                        setViewVisibility(R.id.material_battery_widget_bluetooth_name_textview, View.GONE)
+                        setViewVisibility(R.id.material_battery_widget_bluetooth_battery_textview, View.GONE)
+                    }
+                    // 横に長いウィジェット
+                    val mediumView = RemoteViews(remoteView).apply {
+                        setViewVisibility(R.id.material_battery_widget_device_name_textview, View.GONE)
+                        setViewVisibility(R.id.material_battery_widget_device_battery_textview, View.VISIBLE)
+                        setViewVisibility(R.id.material_battery_widget_bluetooth_name_textview, View.GONE)
+                        setViewVisibility(R.id.material_battery_widget_bluetooth_battery_textview, View.VISIBLE)
+                    }
+                    // レスポンシブ
+                    val viewMapping = mapOf(
+                        // width <= 2 , height = 1
+                        SizeF(100f, 100f) to smallView,
+                        // width >= 2 , height = 1
+                        SizeF(200f, 100f) to mediumView,
+                        // width >= 2 , height >= 2
+                        SizeF(200f, 200f) to remoteView
+                    )
+                    val remoteViews = RemoteViews(viewMapping)
+                    appWidgetManager.updateAppWidget(id, remoteViews)
+                } else {
+                    // 更新
+                    appWidgetManager.updateAppWidget(id, remoteView)
+                }
             }
         }
     }
