223174094e1a50762f2d86de9965abe8d549915c
takusan23 <takumu1200@gmail.com>
2023-08-21 03:00:47 +0900

    ウィジェットの縦並び版も安定してそうなので公開する。 targetSdk も更新したのでそれも公開。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.takusan23.materialbatterywidget"
         minSdk = 21
         targetSdk = 33
-        versionCode = 1
-        versionName = "1.0.0"
+        versionCode = 2
+        versionName = "1.1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
