f430c7cb6c9a46b29782a91289172ad6b67bcb98
takusan23 <takumu1200@gmail.com>
2024-07-27 01:33:58 +0900

    1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "io.github.takusan23.materialbatterywidget"
         minSdk = 21
         targetSdk = 34
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
